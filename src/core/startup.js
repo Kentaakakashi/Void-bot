0ce221119764aa8571f812c976189fccb09b9c4f
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-09-19 23:53:05 +0530

    Format Phase 1 file: src/core/startup.js

--- src/core/startup.js
+++ src/core/startup.js
@@ -1,1 +1,65 @@
-const path=require("path"),{REST,Routes}=require("discord.js"),{createClient}=require("./client"),{loadCommands}=require("./commandLoader"),{loadEvents}=require("./eventLoader"),{initializeDatabase}=require("../database/firestore"),config=require("../config/config"),logger=require("../utils/logger");async function registerCommands(commands){const rest=new REST({version:"10"}).setToken(config.discord.token),body=[...commands.values()].map(c=>c.data.toJSON());await rest.put(Routes.applicationGuildCommands(config.discord.clientId,config.discord.guildId),{body});logger.info("Registered "+body.length+" guild command(s).");}async function start(){initializeDatabase();const client=createClient(),commands=loadCommands(path.join(__dirname,"..","commands"));client.commands=commands;loadEvents(client,path.join(__dirname,"..","events"));await registerCommands(commands);await client.login(config.discord.token);}module.exports={start};
+const path = require("path");
+
+const {
+  REST,
+  Routes
+} = require("discord.js");
+
+const {
+  createClient
+} = require("./client");
+
+const {
+  loadCommands
+} = require("./commandLoader");
+
+const {
+  loadEvents
+} = require("./eventLoader");
+
+const {
+  initializeDatabase
+} = require("../database/firestore");
+
+const config = require("../config/config");
+const logger = require("../utils/logger");
+
+async function registerCommands(commands) {
+  const rest = new REST({ version: "10" }).setToken(config.discord.token);
+
+  const body = [...commands.values()].map((command) => command.data.toJSON());
+
+  await rest.put(
+    Routes.applicationGuildCommands(
+      config.discord.clientId,
+      config.discord.guildId
+    ),
+    { body }
+  );
+
+  logger.info(`Registered ${body.length} guild command(s).`);
+}
+
+async function start() {
+  initializeDatabase();
+
+  const client = createClient();
+
+  const commands = loadCommands(
+    path.join(__dirname, "..", "commands")
+  );
+
+  client.commands = commands;
+
+  loadEvents(
+    client,
+    path.join(__dirname, "..", "events")
+  );
+
+  await registerCommands(commands);
+  await client.login(config.discord.token);
+}
+
+module.exports = {
+  start
+};
