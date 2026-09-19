63ad4e255d40450bd4e16c80ccbd9822af564431
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-09-19 23:48:26 +0530

    VØID Phase 1 foundation: src/core/client.js

---
+++ src/core/client.js
@@ -0,0 +1,1 @@
+const {Client,GatewayIntentBits}=require("discord.js");function createClient(){return new Client({intents:[GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent]});}module.exports={createClient};
